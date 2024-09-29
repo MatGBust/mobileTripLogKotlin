9f50bc10ee1a62471925844bded1f1623f7335c4
roryrod40 <rodriguez.rory40@gmail.com>
2024-09-28 22:38:26 -0400

    Creates home page in main activity and logs a message when map or list view image is pressed

--- app/src/main/java/com/example/triplogger/MainActivity.kt
+++ app/src/main/java/com/example/triplogger/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.triplogger
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,35 +17,30 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.triplogger.ui.theme.TripLoggerTheme
 
+private const val TAG = "MainActivity"
 class MainActivity : ComponentActivity() {
+    private lateinit var ivMapViewIcon : ImageView
+    private  lateinit var  ivListViewIcon : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            TripLoggerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_main)
+
+        ivMapViewIcon = findViewById(R.id.ivMapViewIcon)
+        ivListViewIcon = findViewById(R.id.ivListIcon)
+        ivMapViewIcon.setOnClickListener(object : OnClickListener {
+            override fun onClick(p0: View?) {
+                Log.i(TAG, "ivMapViewIcon clicked ")
             }
-        }
+        })
+        ivListViewIcon.setOnClickListener(object : OnClickListener {
+            override fun onClick(p0: View?) {
+                Log.i(TAG, "ivListViewIcon clicked ")
+            }
+        })
+
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TripLoggerTheme {
-        Greeting("Android")
-    }
-}
