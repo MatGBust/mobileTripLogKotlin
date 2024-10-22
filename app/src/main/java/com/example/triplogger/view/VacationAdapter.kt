434269eebb4af6441dd40906ea080b0807ea58db
Jonathan Curren <curren.34@buckeyemail.osu.edu>
2024-10-22 14:40:06 -0400

    Removed comments

--- app/src/main/java/com/example/triplogger/view/VacationAdapter.kt
+++ app/src/main/java/com/example/triplogger/view/VacationAdapter.kt
@@ -48,8 +48,14 @@ class VacationAdapter(val vacationList: MutableList<Vacation>,
         }
 
         holder.updateButton.setOnClickListener {
-            val intent = Intent(holder.itemView.context, UpdateVacationActivity::class.java)
-            intent.putExtra("VACATION_TITLE", vacation.title)
+            val intent = Intent(holder.itemView.context, UpdateVacationActivity::class.java).apply {
+                putExtra("VACATION_TRIP_ID", vacation.tripId) // Pass tripId
+                putExtra("VACATION_TITLE", vacation.title)
+                putExtra("VACATION_DESCRIPTION", vacation.description)
+                putExtra("VACATION_LOCATION", vacation.location)
+                putExtra("VACATION_DATE", vacation.date)
+                putExtra("VACATION_NOTES", vacation.notes)
+            }
             holder.itemView.context.startActivity(intent)
         }
     }
